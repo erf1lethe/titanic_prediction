34652c96178a873f90a96e77e8704e16cc945659
erf1lethe <erfilethe@hotmail.com>
2025-01-29 16:50:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.feature_extraction import DictVectorizer
 
 # Cargar el modelo y el DictVectorizer
-with open('churn.pck', 'rb') as f:
+with open('model.pck', 'rb') as f:
     dv, model = pickle.load(f)
 
 
